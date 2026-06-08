e44d621c0ee9d0fdaa11621863c0872a84e21e72
amar1ad <amar12ad6@gmail.com>
2026-06-08 22:05:18 +0300

    Updated project file: app/src/main/java/com/ammarad/dictionary/BootReceiver.java via SketchGit

---
+++ app/src/main/java/com/ammarad/dictionary/BootReceiver.java
@@ -0,0 +1,15 @@
+package com.ammarad.dictionary;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent serviceIntent = new Intent(context, DailyNotificationService.class);
+            context.startService(serviceIntent);
+        }
+    }
+}
